1d36c3e1abaa1ac208ed8bc1e8c97346e1addcb0
Yousef-Eleshy <yousef.medhat@hotmail.com>
2020-06-13 15:03:46 +0000

    Error Fix Attempt

--- texmar_collected/models/.ipynb_checkpoints/customer_order-checkpoint.py
+++ texmar_collected/models/.ipynb_checkpoints/customer_order-checkpoint.py
@@ -71,7 +71,7 @@ class delivery_fun (models.Model):
             'location_id': self.mapped ('location_id').id,
             'location_dest_id': self.mapped ('location_dest_id').id,
             'customer_order_delivery': self.env['sale.order'].search (
-                ['|', ('name', '=', self.origin), ('client_order_ref', '!=', False)], limit=1).customer_order
+                ['|', ('name', '=', self.origin), ('client_order_ref', '!=', False)], limit=1)[0].customer_order
         }
 
 
